f6029bb58c9ed1b65fa27280b85e3c061291fa63
aldy-san <nsmaulidi12@gmail.com>
2020-12-24 10:34:39 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    purge: ['./public/**/*.html', './public/**/*.js'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: "#4E3EC8",
+                secondary: "#FFBF3A",
+                ternary: "#F68059",
+                dark: "#161616",
+                light: "#F0F1FA"
+            }
+        },
+    },
+    variants: {
+        padding: ['responsive', 'hover']
+    },
+    plugins: [],
+}
